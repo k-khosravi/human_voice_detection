1491f20d55fe2b31f33a98b129d2effd032a1908
Kosar Khosravi <kosar.khosravi1378@gmail.com>
2021-09-16 11:16:42 +0430

    second commit

---
+++ preprocess/preprocess.py
@@ -0,0 +1,29 @@
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.utils import to_categorical
+
+# Second dimension of the feature is dim2
+feature_dim_2 = 11
+
+# Save data to array file first
+save_data_to_array(max_len=feature_dim_2)
+
+# # Loading train set and test set
+X_train, X_test, y_train, y_test = get_train_test()
+
+# # Feature dimension
+feature_dim_1 = 20
+channel = 1
+epochs = 50
+batch_size = 100
+verbose = 1
+num_classes = 3
+
+# Reshaping to perform 2D convolution
+X_train = X_train.reshape(X_train.shape[0], feature_dim_1, feature_dim_2, channel)
+X_test = X_test.reshape(X_test.shape[0], feature_dim_1, feature_dim_2, channel)
+
+y_train_hot = to_categorical(y_train)
+y_test_hot = to_categorical(y_test)
